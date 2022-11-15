ec598419ea24e18b2135d24dd60ea2d11e44e51c
Soh Jun Wen <junwen.soh@thecarousell.com>
2022-11-16 07:54:14 +0800

    Use env var for PORT

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello from Node, from inside a docker container...</h1>");
 });
 
-app.listen(8080, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log("App running on port 8080...");
 });
